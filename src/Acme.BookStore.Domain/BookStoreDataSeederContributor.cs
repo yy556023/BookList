815cd6c525170086fa1366455f12c358b58ecb4b
Yuu <yy556023@gmail.com>
2023-04-11 22:22:03 +0800

    新增種子資料

---
+++ src/Acme.BookStore.Domain/BookStoreDataSeederContributor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Threading.Tasks;
+using Acme.BookStore.Books;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore
+{
+    public class BookStoreDataSeederContributor
+        : IDataSeedContributor, ITransientDependency
+    // ABP的生命週期 可以繼承 ISingletonDependency, IScopedDependency, ITransientDependency 效果相當於 StartUp註冊生命週期的AddScoped, AddSingleton, AddTransient
+    {
+        private readonly IRepository<Book, Guid> _bookRepository;
+
+        // 於建構式注入 倉儲 IRepository<Book, Guid> 實體為Book 主鍵為Guid
+        public BookStoreDataSeederContributor(IRepository<Book, Guid> bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public async Task SeedAsync(DataSeedContext context)
+        {
+            if (await _bookRepository.GetCountAsync() <= 0)
+            {
+                await _bookRepository.InsertAsync(
+                    new Book
+                    {
+                        Name = "1984",
+                        Type = BookType.Dystopia,
+                        PublishDate = new DateTime(1949, 6, 8),
+                        Price = 19.84f
+                    },
+                    autoSave: true
+                );
+
+                await _bookRepository.InsertAsync(
+                    new Book
+                    {
+                        Name = "The Hitchhiker's Guide to the Galaxy",
+                        Type = BookType.ScienceFiction,
+                        PublishDate = new DateTime(1995, 9, 27),
+                        Price = 42.0f
+                    },
+                    autoSave: true
+                );
+            }
+        }
+    }
+}
