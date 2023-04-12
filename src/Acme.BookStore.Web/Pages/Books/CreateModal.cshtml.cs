151e418369698748b65af6c173cc3225ec218ce0
Yuu <yy556023w@gmail.com>
2023-04-12 11:27:11 +0800

    新增CreateModal 畫面

---
+++ src/Acme.BookStore.Web/Pages/Books/CreateModal.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Acme.BookStore.Books;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Acme.BookStore.Web.Pages.Books
+{
+    public class CreateModalModel : BookStorePageModel
+    {
+        [BindProperty] // Book 属性上的 [BindProperty] 特性將post request提交上来的資料綁定到該属性上.
+        public CreateUpdateBookDto Book { get; set; }
+
+        private readonly IBookAppService _bookAppService;
+
+        public CreateModalModel(IBookAppService bookAppService)
+        {
+            _bookAppService = bookAppService;
+        }
+
+        public void OnGet()
+        {
+            Book = new CreateUpdateBookDto();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            await _bookAppService.CreateAsync(Book);
+            return NoContent();
+        }
+    }
+}
