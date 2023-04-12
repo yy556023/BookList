8723fd68d561b51a62665e02ce98415fa8580db7
Yuu <yy556023w@gmail.com>
2023-04-12 10:16:57 +0800

    新增 IBookAppService 介面

---
+++ src/Acme.BookStore.Application.Contracts/Books/IBookAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Acme.BookStore.Books
+{
+    public interface IBookAppService :
+        ICrudAppService< //Defines CRUD methods 定義了基礎CRUD的方法
+            BookDto, //Used to show books 用於在Interface Layer 顯示Book資訊
+            Guid, //Primary key of the book entity 實體Book的主鍵
+            PagedAndSortedResultRequestDto, //Used for paging/sorting
+            CreateUpdateBookDto> //Used to create/update a book
+    {
+
+    }
+}
