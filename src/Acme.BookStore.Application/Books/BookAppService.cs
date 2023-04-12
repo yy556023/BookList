b92099afb4a20019585eeee719ebc5ef88e06276
Yuu <yy556023w@gmail.com>
2023-04-12 10:17:15 +0800

    新增 BookAppService 實作

---
+++ src/Acme.BookStore.Application/Books/BookAppService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore.Books
+{
+    public class BookAppService :
+        CrudAppService<
+            Book, //The Book entity 實體Book
+            BookDto, //Used to show books Book的DTO
+            Guid, //Primary key of the book entity 實體Book的主鍵
+            PagedAndSortedResultRequestDto, //Used for paging/sorting
+            CreateUpdateBookDto>, //Used to create/update a book
+        IBookAppService //implement the IBookAppService
+    {
+        // ABP自動為每個聚合根(或實體)創建默認倉儲 <== 這句不懂
+        public BookAppService(IRepository<Book, Guid> repository) // 這邊為建構時 注入倉儲Book
+            : base(repository)
+        {
+
+        }
+    }
+}
