9b27107f5a2e59ca861b1b59003ae8b2cfff3f03
Yuu <yy556023w@gmail.com>
2023-04-12 10:16:35 +0800

    Mapper 實體與DTO

--- src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
+++ src/Acme.BookStore.Application/BookStoreApplicationAutoMapperProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Acme.BookStore.Books;
+using AutoMapper;
 
 namespace Acme.BookStore;
 
@@ -9,5 +10,11 @@ public class BookStoreApplicationAutoMapperProfile : Profile
         /* You can configure your AutoMapper mapping configuration here.
          * Alternatively, you can split your mapping configurations
          * into multiple profile classes for a better organization. */
+
+        // 由於存在資料庫之Entity Book類別 要傳輸到Interface Layer(表示層) 所以藉由AutoMapper函示庫自動轉換
+        // Ex: CreateMap<來源,目標>();
+        CreateMap<Book, BookDto>();
+        // 由於會經由前端傳送資訊來更新 Book實體 所以此處使用AutoMapper自動轉換為Book
+        CreateMap<CreateUpdateBookDto, Book>();
     }
 }
