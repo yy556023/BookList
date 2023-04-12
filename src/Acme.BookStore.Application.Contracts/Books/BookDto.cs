9607c64eefa3a3c6a1e3e73fa4ea050a9578e530
Yuu <yy556023w@gmail.com>
2023-04-12 10:15:46 +0800

    新增BookDto

---
+++ src/Acme.BookStore.Application.Contracts/Books/BookDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.BookStore.Books
+{
+    public class BookDto : AuditedEntityDto<Guid> // 為何DTO要繼承AuditedEntityDto而不是跟Entity一樣繼承AuditedAggregateRoot(聚合根)
+    {
+        public string Name { get; set; }
+
+        public BookType Type { get; set; }
+
+        public DateTime PublishDate { get; set; }
+
+        public float Price { get; set; }
+    }
+}
