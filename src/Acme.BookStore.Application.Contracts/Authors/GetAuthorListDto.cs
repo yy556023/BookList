ef2b6a7dca66e9d92eeccd34ba34f1360aa1e8d1
Yuu <yy556023w@gmail.com>
2023-04-13 11:46:21 +0800

    新增GetAuthorListDto

---
+++ src/Acme.BookStore.Application.Contracts/Authors/GetAuthorListDto.cs
@@ -0,0 +1,11 @@
+﻿using Volo.Abp.Application.Dtos;
+
+namespace Acme.BookStore.Authors
+{
+    // PagedAndSortedResultRequestDto 具有標準的分頁和排序属性: int MaxResultCount, int SkipCount 和 string Sorting.
+    public class GetAuthorListDto : PagedAndSortedResultRequestDto
+    {
+        // Filter 用於查詢作者(sorting Method). 它可以是 null 或 string.Empty 以獲得所有用戶.
+        public string Filter { get; set; }
+    }
+}
