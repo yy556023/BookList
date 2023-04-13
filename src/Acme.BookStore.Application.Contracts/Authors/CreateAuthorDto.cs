970fce41079ae88dad5a9e2306b4e2bde82cd5c2
Yuu <yy556023w@gmail.com>
2023-04-13 11:48:11 +0800

    新增Create和Update的Dto

---
+++ src/Acme.BookStore.Application.Contracts/Authors/CreateAuthorDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Acme.BookStore.Authors
+{
+    public class CreateAuthorDto
+    {
+        [Required] // 這些標籤可以用來驗證DTO的正確性
+        [StringLength(AuthorConsts.MaxNameLength)]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+
+        public string ShortBio { get; set; }
+    }
+}
