c6fd5d1c47652f9285e8fdc6616f8b3e501c2bb3
Yuu <yy556023w@gmail.com>
2023-04-14 09:05:00 +0800

    新增Author畫面及js

---
+++ src/Acme.BookStore.Web/Pages/Authors/Index.js
@@ -0,0 +1,87 @@
+﻿$(function () {
+    var l = abp.localization.getResource('BookStore');
+
+    // abp.appPath 定義在Web/wwwroot/libs/abp/core/abp.js
+    var createModal = new abp.ModalManager(abp.appPath + 'Authors/CreateModal');
+    var editModal = new abp.ModalManager(abp.appPath + 'Authors/EditModal');
+
+    // 官方文件：https://docs.abp.io/en/abp/latest/UI/AspNetCore/Data-Tables
+    var dataTable = $('#AuthorsTable').DataTable(
+        abp.libs.datatables.normalizeConfiguration({
+            serverSide: true,
+            paging: true,
+            order: [[1, "asc"]],
+            searching: false,
+            scrollX: true,
+            ajax: abp.libs.datatables.createAjax(acme.bookStore.authors.author.getList),
+            columnDefs: [
+                {
+                    title: l('Actions'),
+                    rowAction: {
+                        items:
+                            [
+                                {
+                                    text: l('Edit'),
+                                    visible:
+                                        abp.auth.isGranted('BookStore.Authors.Edit'),
+                                    action: function (data) {
+                                        editModal.open({ id: data.record.id });
+                                    }
+                                },
+                                {
+                                    text: l('Delete'),
+                                    visible:
+                                        abp.auth.isGranted('BookStore.Authors.Delete'),
+                                    confirmMessage: function (data) {
+                                        return l(
+                                            'AuthorDeletionConfirmationMessage',
+                                            data.record.name
+                                        );
+                                    },
+                                    action: function (data) {
+                                        acme.bookStore.authors.author
+                                            .delete(data.record.id)
+                                            .then(function () {
+                                                abp.notify.info(
+                                                    l('SuccessfullyDeleted')
+                                                );
+                                                dataTable.ajax.reload();
+                                            });
+                                    }
+                                }
+                            ]
+                    }
+                },
+                {
+                    title: l('Name'),
+                    data: "name"
+                },
+                {
+                    title: l('BirthDate'),
+                    data: "birthDate",
+                    render: function (data) {
+                        // 使用了luxon函式庫 格式化DateTime值
+                        return luxon
+                            .DateTime
+                            .fromISO(data, {
+                                locale: abp.localization.currentCulture.name
+                            }).toLocaleString();
+                    }
+                }
+            ]
+        })
+    );
+
+    createModal.onResult(function () {
+        dataTable.ajax.reload();
+    });
+
+    editModal.onResult(function () {
+        dataTable.ajax.reload();
+    });
+
+    $('#NewAuthorButton').click(function (e) {
+        e.preventDefault();
+        createModal.open();
+    });
+});
