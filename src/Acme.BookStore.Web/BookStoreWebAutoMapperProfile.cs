a232b5aefde0d2a466fb56df147e6ba79278f684
Yuu <yy556023w@gmail.com>
2023-04-14 10:36:21 +0800

    新增UpdateAuthor映射

--- src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
+++ src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
@@ -13,5 +13,9 @@ public class BookStoreWebAutoMapperProfile : Profile
 
         CreateMap<Pages.Authors.CreateModalModel.CreateAuthorViewModel,
                     CreateAuthorDto>();
+
+        // 由於從Application Layer傳出來時事AuthorDto 所以建立由 AuthorDto 轉換到 EditAuthorViewModel 的映射
+        CreateMap<AuthorDto, Pages.Authors.EditModalModel.EditAuthorViewModel>();
+        CreateMap<Pages.Authors.EditModalModel.EditAuthorViewModel, UpdateAuthorDto>();
     }
 }
