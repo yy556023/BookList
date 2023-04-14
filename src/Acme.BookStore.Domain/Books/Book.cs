59a5f76a07a7d36e12e7f5d5acda92a6c01ed147
Yuu <yy556023w@gmail.com>
2023-04-14 10:49:03 +0800

    Book 新增AuthorId 屬性

--- src/Acme.BookStore.Domain/Books/Book.cs
+++ src/Acme.BookStore.Domain/Books/Book.cs
@@ -8,6 +8,9 @@ namespace Acme.BookStore.Books
                                                    // 相比AggregateRoot類，AuditedAggregateRoot添加了更多属性：CreationTime、CreatorId、LastModificationTime和LastModifierId。
                                                    // 當您將實體体寫入資料庫时​​，ABP 會自動給這些属性赋值，CreationTime會設置為當前時間，CreatorId會自動設置為當前用戶的Id属性。
     {
+        // 為了新增 Book與Author的關係 所以新增AuthorId屬性
+        public Guid AuthorId { get; set; }
+
         public string Name { get; set; }
 
         public BookType Type { get; set; }
