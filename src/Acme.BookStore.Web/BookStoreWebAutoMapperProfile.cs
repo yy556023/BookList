984740f32a1cdc180e7093ea62fe6ff78f03ec34
Yuu <yy556023w@gmail.com>
2023-04-14 10:15:50 +0800

    新增CreateAuthorViewModel到CreateAuthorDto的轉換

--- src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
+++ src/Acme.BookStore.Web/BookStoreWebAutoMapperProfile.cs
@@ -1,4 +1,5 @@
-﻿using Acme.BookStore.Books;
+﻿using Acme.BookStore.Authors;
+using Acme.BookStore.Books;
 using AutoMapper;
 
 namespace Acme.BookStore.Web;
@@ -9,5 +10,8 @@ public class BookStoreWebAutoMapperProfile : Profile
     {
         //Define your AutoMapper configuration here for the Web project.
         CreateMap<BookDto, CreateUpdateBookDto>();
+
+        CreateMap<Pages.Authors.CreateModalModel.CreateAuthorViewModel,
+                    CreateAuthorDto>();
     }
 }
